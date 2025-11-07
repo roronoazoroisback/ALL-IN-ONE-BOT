9d553d6347545429335ce1654edcc0daba861ecc
HELL-Vextorx <164228184+roronoazoroisback@users.noreply.github.com>
2025-11-08 01:50:39 +0530

    Update lavalink.js

--- lavalink.js
+++ lavalink.js
@@ -1,11 +1,10 @@
 module.exports = {
-    enabled: true, 
-    lavalink: {
-        name: "RailwayNode",
-        password: "password", // or whatever password you set in Railway
-        host: "lavalink-4-production-ec66.up.railway.app", // ✅ no double 'l'
-        port: 2333,
-        secure: false
-    }
+  enabled: false,
+  lavalink: {
+    name: "RailwayNode",
+    password: "",
+    host: "",
+    port: 0,
+    secure: false
+  }
 };
-
